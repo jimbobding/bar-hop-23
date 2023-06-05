4f3db7ac64551dc09da37bac56e20b1d47fde7ee
James Graham David <Jimmy.David@td7mhyf0jl.broadband>
2023-06-05 15:24:24 +0100

    added nav bar with logic and css, not functional links yet and still issues with modile device view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./css/Form.css";
+import "./css/Nav.css";
 import "./App.css";
 import Form from "./components/Form";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <Form />
     </>
   );
